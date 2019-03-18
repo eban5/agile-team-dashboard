8abdccba4db00ff8d34fc5cd6b28fc01980ef3f7
Esteban Amas <estebanamas@gmail.com>
2019-03-18 09:44:21 -0400

    Update dates to fix dates being off by two calendar days

--- src/pi5.js
+++ src/pi5.js
@@ -3,202 +3,242 @@ var data = [{
         "iteration": "Iteration 5.1",
         "current_date": "3/4/19",
         "iteration_day": "1"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/5/19",
         "iteration_day": "2"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/6/19",
         "iteration_day": "3"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/7/19",
         "iteration_day": "4"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/8/19",
         "iteration_day": "5"
-    },{
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/11/19",
         "iteration_day": "6"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/12/19",
         "iteration_day": "7"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/13/19",
         "iteration_day": "8"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/14/19",
         "iteration_day": "9"
-    }, {
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.1",
         "current_date": "3/15/19",
         "iteration_day": "10"
-    }, {
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.2",
+        "current_date": "3/18/19",
+        "iteration_day": "1"
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.2",
+        "current_date": "3/19/19",
+        "iteration_day": "2"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.2",
         "current_date": "3/20/19",
-        "iteration_day": "1"
-    }, {
+        "iteration_day": "3"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.2",
         "current_date": "3/21/19",
-        "iteration_day": "2"
-    }, {
+        "iteration_day": "4"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.2",
         "current_date": "3/22/19",
-        "iteration_day": "3"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.2",
-        "current_date": "3/23/19",
-        "iteration_day": "4"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.2",
-        "current_date": "3/24/19",
         "iteration_day": "5"
-    }, {
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.2",
+        "current_date": "3/25/19",
+        "iteration_day": "6"
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.2",
+        "current_date": "3/26/19",
+        "iteration_day": "7"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.2",
         "current_date": "3/27/19",
-        "iteration_day": "6"
-    }, {
+        "iteration_day": "8"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.2",
         "current_date": "3/28/19",
-        "iteration_day": "7"
-    }, {
+        "iteration_day": "9"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.2",
         "current_date": "3/29/19",
-        "iteration_day": "8"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.2",
-        "current_date": "3/30/19",
-        "iteration_day": "9"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.2",
-        "current_date": "3/31/19",
         "iteration_day": "10"
-    }, {
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.3",
+        "current_date": "4/1/19",
+        "iteration_day": "1"
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.3",
+        "current_date": "4/2/19",
+        "iteration_day": "2"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.3",
         "current_date": "4/3/19",
-        "iteration_day": "1"
-    }, {
+        "iteration_day": "3"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.3",
         "current_date": "4/4/19",
-        "iteration_day": "2"
-    }, {
+        "iteration_day": "4"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.3",
         "current_date": "4/5/19",
-        "iteration_day": "3"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.3",
-        "current_date": "4/6/19",
-        "iteration_day": "4"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.3",
-        "current_date": "4/7/19",
         "iteration_day": "5"
-    }, {
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.3",
+        "current_date": "4/8/19",
+        "iteration_day": "6"
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.3",
+        "current_date": "4/9/19",
+        "iteration_day": "7"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.3",
         "current_date": "4/10/19",
-        "iteration_day": "6"
-    }, {
+        "iteration_day": "8"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.3",
         "current_date": "4/11/19",
-        "iteration_day": "7"
-    }, {
+        "iteration_day": "9"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.3",
         "current_date": "4/12/19",
-        "iteration_day": "8"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.3",
-        "current_date": "4/13/19",
-        "iteration_day": "9"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.3",
-        "current_date": "4/14/19",
         "iteration_day": "10"
-    }, {
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.4",
+        "current_date": "4/15/19",
+        "iteration_day": "1"
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.4",
+        "current_date": "4/16/19",
+        "iteration_day": "2"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.4",
         "current_date": "4/17/19",
-        "iteration_day": "1"
-    }, {
+        "iteration_day": "3"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.4",
         "current_date": "4/18/19",
-        "iteration_day": "2"
-    }, {
+        "iteration_day": "4"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.4",
         "current_date": "4/19/19",
-        "iteration_day": "3"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.4",
-        "current_date": "4/20/19",
-        "iteration_day": "4"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.4",
-        "current_date": "4/21/19",
         "iteration_day": "5"
-    }, {
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.4",
+        "current_date": "4/22/19",
+        "iteration_day": "6"
+    },
+    {
+        "program_increment": "PI 5",
+        "iteration": "Iteration 5.4",
+        "current_date": "4/23/19",
+        "iteration_day": "7"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.4",
         "current_date": "4/24/19",
-        "iteration_day": "6"
-    }, {
+        "iteration_day": "8"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.4",
         "current_date": "4/25/19",
-        "iteration_day": "7"
-    }, {
+        "iteration_day": "9"
+    },
+    {
         "program_increment": "PI 5",
         "iteration": "Iteration 5.4",
         "current_date": "4/26/19",
-        "iteration_day": "8"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.4",
-        "current_date": "4/27/19",
-        "iteration_day": "9"
-    }, {
-        "program_increment": "PI 5",
-        "iteration": "Iteration 5.4",
-        "current_date": "4/28/19",
         "iteration_day": "10"
     }
 ]
+
 
 export default data;
