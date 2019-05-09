2d786dde418da5ee80ba3169321a4fe56333c29b
Esteban Amas <estebanamas@gmail.com>
2019-05-08 23:06:02 -0400

    Updated build

---
+++ build/precache-manifest.fc60f90e65b692300d7459a8371b82a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc7f8f5c11f2dbfa2ab401fe501f297e",
+    "url": "/static/media/dashboard-background-wide.fc7f8f5c.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "efaaa065469988344b5a",
+    "url": "/static/js/main.2ef2f0e5.chunk.js"
+  },
+  {
+    "revision": "33375d65d79d401e5811",
+    "url": "/static/js/2.299d48bb.chunk.js"
+  },
+  {
+    "revision": "efaaa065469988344b5a",
+    "url": "/static/css/main.657f8e89.chunk.css"
+  },
+  {
+    "revision": "ca7aa2ce9af86733572e0e1c67e517c5",
+    "url": "/index.html"
+  }
+];
